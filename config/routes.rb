5097384663d8260bb5bfe6882d0aa062f8c1ccc8
petter gulliksen <petter-gulliksen@hotmail.com>
2019-10-01 13:17:27 +0200

    changed the routes and the title in application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :trip, only [:new, :show ] do
-    resources :trip_item, only [ :index, :show, :new ]
+  resources :trips, only: [:new, :show ] do
+    resources :trip_items, only: [ :index, :show, :new ]
   end
 end
