dee2806706087a8d855d7aff9c4e57edf026e3a3
petter gulliksen <petter-gulliksen@hotmail.com>
2019-10-02 16:04:02 +0200

    added name and location to trip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_134529) do
+ActiveRecord::Schema.define(version: 2019_10_02_135648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 2019_10_02_134529) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
+    t.string "name"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
