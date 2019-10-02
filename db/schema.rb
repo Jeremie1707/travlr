f3e2f2bab62dfb073b6c3ec58180746eb55741a8
petter gulliksen <petter-gulliksen@hotmail.com>
2019-10-02 17:48:48 +0200

    implmented map for trip(centering purposes)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_135648) do
+ActiveRecord::Schema.define(version: 2019_10_02_144614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 2019_10_02_135648) do
     t.datetime "updated_at", null: false
     t.string "location"
     t.string "name"
+    t.float "longitude"
+    t.float "latitude"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
