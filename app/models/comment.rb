bbf74cb38318044ee43cfe30ede711c09d28d70d
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-10-06 00:20:21 +0200

    add default map for detail item without picture

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :trip_item
   validates :comment, presence: true
+  validates :content, length: { minimum: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :trips, through: :participants
-  has_many :participants
-  has_many :comments
-  has_many :likes
-  has_many :trip_items
+  has_many :participants, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :trip_items, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
   mount_uploader :photo, PhotoUploader
