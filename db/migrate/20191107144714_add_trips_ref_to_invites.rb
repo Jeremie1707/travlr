45b854c2bfe7a5cc3f21fdb4fe8205e4ed27e078
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2020-02-27 23:26:54 +0100

    db

--- db/migrate/20191107144714_add_trips_ref_to_invites.rb
+++
@@ -1,6 +0,0 @@
-class AddTripsRefToInvites < ActiveRecord::Migration[5.2]
-  def change
-    # remove_column :invites, :trip_id, :integer
-    add_reference :invites, :trip, foreign_key: true
-  end
-end
